21fcfe0b53e1fa849d3ca93d7c72ddf9924cef86
santhiya-bharathi <santhiyabharathi1999@gmail.com>
2022-02-15 11:56:40 +0530

    projects added

--- src/App.js
+++ src/App.js
@@ -3,10 +3,31 @@ import './App.css';
 import { useHistory } from "react-router-dom";
 // import {useState} from "react";
 import { Switch, Route } from "react-router-dom";
-
+import GitHubIcon from '@mui/icons-material/GitHub';
 
 function App() {
   const history = useHistory();
+  const project_details = [{projectname:"Recipe Blog App",
+ details:"here you can explore more recipes and can add recipes if you want to add  and you can delete and edit the recipes",
+  frontend:"https://github.com/santhiya-bharathi/blog-app",
+   backend:"https://github.com/santhiya-bharathi/blog-node",
+    demo:"https://recipe-blog-app.netlify.app/"},
+    {projectname:"Recipe Blog App",
+    details:"here you can explore more recipes and can add recipes if you want to add  and you can delete and edit the recipes",
+     frontend:"https://github.com/santhiya-bharathi/blog-app",
+      backend:"https://github.com/santhiya-bharathi/blog-node",
+       demo:"https://recipe-blog-app.netlify.app/"},
+       {projectname:"Recipe Blog App",
+       details:"here you can explore more recipes and can add recipes if you want to add  and you can delete and edit the recipes",
+        frontend:"https://github.com/santhiya-bharathi/blog-app",
+         backend:"https://github.com/santhiya-bharathi/blog-node",
+          demo:"https://recipe-blog-app.netlify.app/"},
+          {projectname:"Recipe Blog App",
+          details:"here you can explore more recipes and can add recipes if you want to add  and you can delete and edit the recipes",
+           frontend:"https://github.com/santhiya-bharathi/blog-app",
+            backend:"https://github.com/santhiya-bharathi/blog-node",
+             demo:"https://recipe-blog-app.netlify.app/"}]
+
   return (
     
       <div className="App">
@@ -44,7 +65,7 @@ function App() {
         </Route>
 
         <Route path="/project">
-          <Project />
+          <Project project_details={project_details}/>
         </Route>
 
         <Route path="/contact">
@@ -104,6 +125,11 @@ return(
     <div className='email-mobile-div'>
     <a className='resume-link' rel="noreferrer" target="_blank" href='https://docs.google.com/document/d/1Xma61efahpOY3Na5mozg-4Ol2rDkopDFlRMcEId4HI8/edit'>Resume docs link</a>
     </div>
+    <br></br>
+    <h1 className='contact-me'>GitHub Link</h1>
+    <div className='email-mobile-div'>
+    <a className='resume-link' rel="noreferrer" target="_blank" href='https://github.com/santhiya-bharathi'>GitHub link</a>
+    </div>
   </div>
 );
 }
@@ -112,12 +138,18 @@ function Skills(){
   return(
     <div className='skills-dev'>
       <h1 className='contact-me'>Skills</h1>
-      <h1 className='contact-me'>Platforms</h1>
+
+      <h1 className='contact-me'>GitHub Profile Link</h1>
+    <div className='email-mobile-div'>
+    <a className='resume-link' rel="noreferrer" target="_blank" href='https://github.com/santhiya-bharathi'>GitHub Profile link</a>
+    </div>
+    
+      <h1 className='contact-me'>Platforms - <span className='skills-data'>(React-Native)</span></h1>
       <div className='img-div'>
       <img  className='img-size' src='https://reactjs.org/logo-og.png' alt='React-Native'/>
       </div>
 
-      <h1 className='contact-me'>Web Front End</h1>
+      <h1 className='contact-me'>Web Front End - <span className='skills-data'>(HTML, CSS, React)</span></h1>
       <div className='img-div'>
      
       <img  className='img-size' src='https://cdn.pixabay.com/photo/2017/08/05/11/16/logo-2582748_1280.png' alt='HTML'/>
@@ -126,18 +158,18 @@ function Skills(){
 
       </div>
 
-      <h1 className='contact-me'>Web Back End</h1>
+      <h1 className='contact-me'>Web Back End - <span className='skills-data'>(Node.js)</span></h1>
       <div className='img-div'>
       <img  className='img-size' src='https://cdn-icons-png.flaticon.com/512/919/919825.png' alt='Node.js'/>
       </div>
 
-      <h1 className='contact-me'>Databases</h1>
+      <h1 className='contact-me'>Databases - <span className='skills-data'>(MongoDB, MySQL)</span></h1>
       <div className='img-div'>
       <img  className='img-size' src='https://digital.ai/sites/default/files/pictures/styles/maxwidth_300/public/pt_logos/mongodb.png?itok=pYTrNt-U' alt='MongoDB'/>
       <img  className='img-size' src='https://e7.pngegg.com/pngimages/700/692/png-clipart-mysql-mysql-thumbnail.png' alt='MySQL'/>
       </div>
 
-      <h1 className='contact-me'>Programming Languages</h1>
+      <h1 className='contact-me'>Programming Languages - <span className='skills-data'>(JavaScript)</span></h1>
       <div className='img-div'>
       <img  className='img-size' src='https://ih1.redbubble.net/image.1091999270.7282/flat,750x,075,f-pad,750x1000,f8f8f8.jpg' alt='JavaScript'/>
       </div>
@@ -146,30 +178,52 @@ function Skills(){
   );
 }
 
-
-function Project(){
+function Project({project_details}){
   return(
     <div className='skills-dev'>
      <h1 className='contact-me'>Projects</h1>
-
-     <div className='project-mobile-div'>
-       <div className='container'>
-     <p className='project-name'>Project Name</p>
-     <p className='about-div'>Project brief explanation Project brief explanation Project brief explanationProject brief explanation Project brief explanation</p>
-   
-     <p className='project-name'>Front-end & Back-end gitHub and demo Links</p>
-    <div className='logo-img-div'>
-      <img className='logo-link-img' src='https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQQFd8OQgaQxY_6_GnWY6sIkyXm-MfDbnZpl0cQ7ijItv6mN3SkDi3vmrmEuO7gywrwalg&usqp=CAU' alt='Frondend'/>
-      <img className='logo-link-img' src='https://www.pngfind.com/pngs/m/206-2063047_smart-data-transform-back-end-systems-icon-png.png' alt='Backend'/>
-      <img className='logo-link-img' src='https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcSrs0rMh22IqEyJb35IAKRhum-_eZIlH3IMGXwgjX_DvGUHijc--zqjy42et6hmOeI7bRA&usqp=CAU' alt='Live Demo'/>
+     <div className='project-grid-margin'>
+    {project_details.map(({projectname,details,frontend,backend,demo,id})=>(<ProjectDetails key={id} projectname={projectname} details={details} frontend={frontend} backend={backend} demo={demo}/>))}
+     
     </div>
-
-     </div>
-     </div>
-
     </div>
   );
 }
+
+function ProjectDetails({projectname,details,frontend,backend,demo}){
+  return(
+   
+     <div className='project-grid'>
+
+      <div className='container'>
+     <p className='project-name'>{projectname}</p>
+     <p className='about-div'>{details}</p>
+   
+     <p className='project-name'>Front-end & Back-end gitHub and demo Links</p>
+    <div className='logo-img-div'>
+
+      <a rel="noreferrer" target="_blank" href={frontend}>
+      <img className='logo-link-img' src='https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQQFd8OQgaQxY_6_GnWY6sIkyXm-MfDbnZpl0cQ7ijItv6mN3SkDi3vmrmEuO7gywrwalg&usqp=CAU' alt='Frondend'/>
+      </a>
+
+      <a rel="noreferrer" target="_blank" href={backend}>
+      <img className='logo-link-img' src='https://www.pngfind.com/pngs/m/206-2063047_smart-data-transform-back-end-systems-icon-png.png' alt='Backend'/>
+      </a>
+      
+      <a rel="noreferrer" target="_blank" href={demo}>
+      <img className='logo-link-img' src='https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcSrs0rMh22IqEyJb35IAKRhum-_eZIlH3IMGXwgjX_DvGUHijc--zqjy42et6hmOeI7bRA&usqp=CAU' alt='Live Demo'/>
+      </a>
+     
+    </div>
+
+     </div>
+     </div>
+    
+   
+  );
+}
+
+
 
 function Contact(){
   return(
@@ -186,6 +240,15 @@ function Contact(){
       <h2 className='contact-me-heading'>Mobile.No</h2>
       <p className='contact-me-heading-det'>9362633683</p>
       </div>
+      
+     
+    <div>
+    <h1 className='git-name-color'>GitHub Profile Link</h1>
+    <div className='flex-git'>
+    <GitHubIcon/>
+    <a className='resume-link' rel="noreferrer" target="_blank" href='https://github.com/santhiya-bharathi'>GitHub Profile link</a>
+    </div>
+    </div>
       </div>
     </div>
   );
