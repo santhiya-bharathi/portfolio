c95e01c7c266b153f1818c1383870ea2e5adda05
santhiya <santhiyabharathi1999@gmail.com>
2022-04-30 21:12:55 +0530

    project added

---
+++ src/components/TopContent/TopContent.js
@@ -0,0 +1,64 @@
+import React from 'react';
+import { Link } from 'react-scroll';
+import './TopContent.css';
+import Typewriter from 'typewriter-effect';
+
+const TopContent = () => {
+    return(
+        <div className='home'>
+
+        <div className='home-flex'>
+        
+        <div className='home-details'>
+            <p className='hello-home'>Hello!</p>
+            <h1 className='name-home'>I'm Santhiya Bharathi</h1>
+            <div className='type'>
+            <h1 >
+                        {
+                            <Typewriter
+
+                                options={{
+                                    strings: [`A Full Stack Developer `,`MERN Stack Developer`,`A Passionate Programmer `],
+                                    autoStart: true,
+                                    loop: true,
+                                    cursorClassName: 'Typewriter__cursor',
+                                    cursor: ''
+                                }}
+                
+                            />
+                        }
+                        </h1>
+                        </div>
+            
+            <div className='pro-con-button'>
+            <a rel="noreferrer" target="_blank" href='https://drive.google.com/file/d/16i0dHCRGJTau-tP4KMwdZTN7nE6SDgNV/view?usp=sharing'>
+                <button className='topcontent__downloadButton'>Resume</button>
+                </a>
+            <Link to='projects' smooth={true} duration={500}>
+         <button className='topcontent__workButton'>My Work</button>
+         </Link>
+           
+            
+            </div>
+            </div>
+        
+          
+            </div>
+                 {/* <div className='topcontent'>
+         <div className='topcontent__container'>
+          <h1>Santhiya Bharathi</h1>
+          <p>Full Stack Developer</p>
+          <a href='www.google.com'>
+             <button className='topcontent__downloadButton'>Download CV</button>
+         </a>
+         <Link to='projects' smooth={true} duration={500}>
+         <button className='topcontent__workButton'>My Work</button>
+         </Link>
+         </div>
+     </div> */}
+            </div>
+       
+    );
+}
+
+export default TopContent;
---
+++ src/components/projectscontainer/projectcontainer.js
@@ -0,0 +1,220 @@
+import React from 'react';
+import { Element } from 'react-scroll';
+import './projectcontainer.css';
+import GitHubIcon from '@mui/icons-material/GitHub';
+import LinkIcon from '@mui/icons-material/Link';
+
+
+const ProjectContainer = () => {
+
+    return(
+   <Element className='projectcontainer' id='projects'>
+ <div className='project-div'>
+    <div className='div-flex'>
+
+    
+
+      
+
+      <div className='container'>
+      <h2 className='project-name'>Movie App</h2>
+      <br />
+        <div className="full-det">
+      
+         <img className="user-pic" src= 'https://gdj-inr5u0ip5pewom.stackpathdns.com/wp-content/uploads/2011/04/css-xhtml-templates-1.jpg' alt='website'/>
+        
+      
+       <div className='details details--blur'>
+           
+        <p className='details-text'>Movie Application has features like user can access and they can post, edit and delete their movie collection and it has star ratings, and you can view more details and trailer of the movie.</p>
+      
+        <p className='details-text'>Technologies Used: React, node.js, mongoDB, Material UI</p>
+        <div className='div-button'>
+      
+      <div>
+        <a className='frontend-link' target="_blank" rel="noreferrer" href='https://github.com/santhiya-bharathi/movie-app'>
+          <div className='button-link'>
+            <GitHubIcon />
+           <a className='frontend-link' target="_blank" rel="noreferrer" href='https://github.com/santhiya-bharathi/movie-app'>Frontend</a>
+          </div>
+          </a>
+          </div>
+
+          <div>
+          <a className='frontend-link' target="_blank" rel="noreferrer" href='https://github.com/santhiya-bharathi/movie-node'>
+          <div className='button-link'>
+          <GitHubIcon />
+          <a className='frontend-link' target="_blank" rel="noreferrer" href='https://github.com/santhiya-bharathi/movie-node'>Backend</a>
+          </div>
+          </a>
+          </div>
+
+          <div>
+          <a className='frontend-link' target="_blank" rel="noreferrer" href='https://movies-app-blog.netlify.app'>
+          <div className='button-link'>
+            <LinkIcon />
+          <a className='frontend-link' target="_blank" rel="noreferrer" href='https://movies-app-blog.netlify.app'>Demolink</a>
+          </div>
+          </a>
+          </div>
+          
+          </div>
+          
+        </div>
+        </div>
+      </div>
+
+      
+
+      <div className='container'>
+      <h2 className='project-name'>Stackoverflow</h2>
+      <br />
+        <div className="full-det">
+      
+         <img className="user-pic" src= 'https://miro.medium.com/max/775/1*yTdreaXaKNbiVM5-lgj-0w.png' alt='website'/>
+        
+      
+       <div className='details details--blur'>
+           
+        <p className='details-text'>In this application user can view answers and edit answer and can ask questions.</p>
+      
+        <p className='details-text'>Technologies Used: React, node.js, mongoDB, Material UI</p>
+        <div className='div-button'>
+      
+      <div>
+        <a className='frontend-link' target="_blank" rel="noreferrer" href='https://github.com/santhiya-bharathi/stackoverflow-app'>
+          <div className='button-link'>
+            <GitHubIcon />
+           <a className='frontend-link' target="_blank" rel="noreferrer" href='https://github.com/santhiya-bharathi/stackoverflow-app'>Frontend</a>
+          </div>
+          </a>
+          </div>
+
+          <div>
+          <a className='frontend-link' target="_blank" rel="noreferrer" href='https://github.com/santhiya-bharathi/stackoverflow-node'>
+          <div className='button-link'>
+          <GitHubIcon />
+          <a className='frontend-link' target="_blank" rel="noreferrer" href='https://github.com/santhiya-bharathi/stackoverflow-node'>Backend</a>
+          </div>
+          </a>
+          </div>
+
+          <div>
+          <a className='frontend-link' target="_blank" rel="noreferrer" href='https://app-stack-overflow.netlify.app'>
+          <div className='button-link'>
+            <LinkIcon />
+          <a className='frontend-link' target="_blank" rel="noreferrer" href='https://app-stack-overflow.netlify.app'>Demolink</a>
+          </div>
+          </a>
+          </div>
+          
+          </div>
+          
+        </div>
+        </div>
+      </div>
+     
+      <div className='container'>
+      <h2 className='project-name'>News-hunt App</h2>
+      <br />
+        <div className="full-det">
+      
+         <img className="user-pic" src= 'https://d1n9mtec73gj9o.cloudfront.net/uploads/85/images/single/newsportal_mini.jpg?1368051456' alt='website'/>
+        
+      
+       <div className='details details--blur'>
+           
+        <p className='details-text'>In News-hunt app, user can read, edit and delete post and like other users post</p>
+      
+        <p className='details-text'>Technologies Used: React, node.js, mongoDB, Material UI</p>
+        <div className='div-button'>
+      
+      <div>
+        <a className='frontend-link' target="_blank" rel="noreferrer" href='https://github.com/santhiya-bharathi/blogger-client'>
+          <div className='button-link'>
+            <GitHubIcon />
+           <a className='frontend-link' target="_blank" rel="noreferrer" href='https://github.com/santhiya-bharathi/blogger-client'>Frontend</a>
+          </div>
+          </a>
+          </div>
+
+          <div>
+          <a className='frontend-link' target="_blank" rel="noreferrer" href='https://github.com/santhiya-bharathi/blogger-server'>
+          <div className='button-link'>
+          <GitHubIcon />
+          <a className='frontend-link' target="_blank" rel="noreferrer" href='https://github.com/santhiya-bharathi/blogger-server'>Backend</a>
+          </div>
+          </a>
+          </div>
+
+          <div>
+          <a className='frontend-link' target="_blank" rel="noreferrer" href='https://news-hunt.netlify.app'>
+          <div className='button-link'>
+            <LinkIcon />
+          <a className='frontend-link' target="_blank" rel="noreferrer" href='https://news-hunt.netlify.app'>Demolink</a>
+          </div>
+          </a>
+          </div>
+          
+          </div>
+          
+        </div>
+        </div>
+      </div>
+
+
+      <div className='container'>
+      <h2 className='project-name'>Blog App</h2>
+      <br />
+        <div className="full-det">
+      
+         <img className="user-pic" src= 'https://www.templateshub.net/uploads/1555508754%20green-nature-environmental-template-min.png' alt='website'/>
+        
+      
+       <div className='details details--blur'>
+           
+        <p className='details-text'>In blog app, user who can view blog and add blog about nature.</p>
+      
+        <p className='details-text'>Technologies Used: React, node.js, mongoDB, Material UI</p>
+        <div className='div-button'>
+      
+      <div>
+        <a className='frontend-link' target="_blank" rel="noreferrer" href='https://github.com/santhiya-bharathi/content-blog'>
+          <div className='button-link'>
+            <GitHubIcon />
+           <a className='frontend-link' target="_blank" rel="noreferrer" href='https://github.com/santhiya-bharathi/content-blog'>Frontend</a>
+          </div>
+          </a>
+          </div>
+
+          <div>
+          <a className='frontend-link' target="_blank" rel="noreferrer" href='https://github.com/santhiya-bharathi/project-node'>
+          <div className='button-link'>
+          <GitHubIcon />
+          <a className='frontend-link' target="_blank" rel="noreferrer" href='https://github.com/santhiya-bharathi/project-node'>Backend</a>
+          </div>
+          </a>
+          </div>
+
+          <div>
+          <a className='frontend-link' target="_blank" rel="noreferrer" href='https://blog-nature.netlify.app'>
+          <div className='button-link'>
+            <LinkIcon />
+          <a className='frontend-link' target="_blank" rel="noreferrer" href='https://blog-nature.netlify.app'>Demolink</a>
+          </div>
+          </a>
+          </div>
+          
+          </div>
+          
+        </div>
+        </div>
+      </div>
+
+    </div>
+    </div>
+   </Element>
+       );
+    }
+    
+    export default ProjectContainer;
